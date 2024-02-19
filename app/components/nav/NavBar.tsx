f7ef2152b232b55e0b338ecd5e8614c67c24c089
Tunde <babatea4real@yahoo.com>
2024-02-19 22:04:09 +0100

    first commit

---
+++ app/components/nav/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Container from "../Container";
+import Link from "next/link";
+import Image from "next/image";
+
+const NavBar = () => {
+  return (
+    <div className="sticky top-0 w-full z-30 ">
+      <div className="py-4">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md:gap-0">
+            <Link href="/">
+              <Image src='/img/default.png' width={150} height={150} alt="home page Logo" />
+            </Link>
+            <div className="md:block hidden">Search Bar</div>
+            <div className="flex items-center gap-8 md:gap-12">
+                <div>Cart</div>
+                <div>User Account</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
