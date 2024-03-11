199e60060d76574a58087ed461b7b3a9a5c56771
Tunde <babatea4real@yahoo.com>
2024-03-11 22:55:34 +0100

    Create product Image

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -2,6 +2,7 @@
 
 import Button from "@/app/Button";
 import ProductColor from "@/app/components/Products/ProductColor";
+import ProductImage from "@/app/components/Products/ProductImage";
 import ProductQuantity from "@/app/components/Products/ProductQuantity";
 import { Rating } from "@mui/material";
 import React, { useCallback, useState } from "react";
@@ -72,7 +73,13 @@ const ProductDetails: React.FC<ProductDetailsProps> = ({ product }) => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-      <div>Images</div>
+      <div>
+        <ProductImage
+          cartProduct={cartProduct}
+          product={product}
+          handleColorSelect={handleColorSelect}
+        />
+      </div>
       <div className="flex flex-col gap-1 text-sm">
         <h2 className="text-3xl font-medium">{product.name}</h2>
         <div className="flex items-center gap-2">
